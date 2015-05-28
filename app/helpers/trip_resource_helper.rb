0d26a9657a4a60033b86235c99b9faaed62e22b9
Florian Sauter <florian.sauter@novatec-gmbh.de>
2015-05-28 21:15:11 +0200

    Improved trip home view.

---
+++ app/helpers/trip_resource_helper.rb
@@ -0,0 +1,11 @@
+module TripResourceHelper
+
+  def fetch_upcoming_events(trip)
+    trip.events.where('start_date > ?', DateTime.now).order(:start_date).limit(5)
+  end
+
+  def fetch_recent_discussions(trip)
+    trip.events.where('start_date > ?', DateTime.now).order(:start_date).limit(5)
+  end
+
+end
